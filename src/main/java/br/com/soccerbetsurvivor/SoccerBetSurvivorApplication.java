42dc09e78de164c964c5d330fa869f0895c23ae2
marceloprof <eck.marcelo@gmail.com>
2020-04-20 23:43:12 +0200

    first commit of the maven project

---
+++ src/main/java/br/com/soccerbetsurvivor/SoccerBetSurvivorApplication.java
@@ -0,0 +1,13 @@
+package br.com.soccerbetsurvivor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerBetSurvivorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerBetSurvivorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/soccerbetsurvivor/SoccerBetSurvivorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.soccerbetsurvivor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerBetSurvivorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
