4eddf1635e3369eb0daea47ec1a2667dc744cb65
afonsoaoc <afonso.aoc@gmail.com>
2020-09-13 19:38:43 -0300

    test container

--- src/test/java/br/com/soccerbetsurvivor/SoccerBetSurvivorApplicationTests.java
+++ src/test/java/br/com/soccerbetsurvivor/SoccerBetSurvivorApplicationTests.java
@@ -5,7 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
-@Testcontainers
 class SoccerBetSurvivorApplicationTests {
 
 
--- src/test/java/br/com/soccerbetsurvivor/repository/UserRespositoryTest.java
+++ src/test/java/br/com/soccerbetsurvivor/repository/UserRespositoryTest.java
@@ -3,7 +3,7 @@ package br.com.soccerbetsurvivor.repository;
 
 import static org.junit.Assert.assertTrue;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringRunner;
